3a918516c99c146196df90a02d8842e32a65d729
punwa <wattanakraimetpannarat@gmail.com>
2025-09-28 16:57:22 +0700

    Update project

---
+++ server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
